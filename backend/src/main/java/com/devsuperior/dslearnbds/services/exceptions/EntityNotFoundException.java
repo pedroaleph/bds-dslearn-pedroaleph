bef1e8312066b2e4b7bbbefa64d241a477996234
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-05-24 17:20:31 -0400

    Validation, Exception handling

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	public EntityNotFoundException (String msg) {
+		super(msg);
+	}
+}
